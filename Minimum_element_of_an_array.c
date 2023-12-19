6b0e20cfd8881c4788150a8a47ad1c6480ef1f2f
23A9CQ61034 <147469653+23A9CQ61034@users.noreply.github.com>
2023-12-19 10:17:00 +0530

    #include<stdio.h>//max int main() { 	int a,i; 	scanf("%d",&a); 	int arr[a]; 	for( i=0; i<a; i++) 	{ 		scanf("%d",&arr[i]); 	} 	int min=arr[0]; 	for( i=0;i<a;i++) 	{ 		if(arr[i]<min) 		{ 		min=arr[i]; 		} 	} 	printf("%d",min); }

---
+++ Minimum_element_of_an_array.c
@@ -0,0 +1,20 @@
+#include<stdio.h>//max
+int main()
+{
+	int a,i;
+	scanf("%d",&a);
+	int arr[a];
+	for( i=0; i<a; i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	int min=arr[0];
+	for( i=0;i<a;i++)
+	{
+		if(arr[i]<min)
+		{
+		min=arr[i];
+		}
+	}
+	printf("%d",min);
+}
