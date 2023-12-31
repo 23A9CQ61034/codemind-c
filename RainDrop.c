c433a2666088f76a31cc9eaf0b26a89800e1f103
23A9CQ61034 <147469653+23A9CQ61034@users.noreply.github.com>
2023-12-31 14:27:07 +0530

    #include <stdio.h> int main() {     int N;     scanf("%d",&N);     if (N%3==0&&N%5!=0&&N%7!=0)     {         printf("Pling");     }     else if (N%5==0&&N%3!=0&&N%7!=0)     {         printf("Plang");         }         else if (N%7==0&&N%3!=0&&N%5!=0)         {             printf("Plong");             }             else if (N%3==0&&N%5==0&&N%7!=0)             {                 printf("PlingPlang");                 }                 else if (N%3!=0&&N%5==0&&N%7==0)                 {                     printf("PlangPlong");                 }                 else if (N%3==0&&N%5!=0&&N%7==0)                 {                     printf("PlingPlong");                 }                 else if (N%3!=0&&N%5!=0&&N%7!=0)                 {                     printf("%d",N);                 } }

---
+++ RainDrop.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+    int N;
+    scanf("%d",&N);
+    if (N%3==0&&N%5!=0&&N%7!=0)
+    {
+        printf("Pling");
+    }
+    else if (N%5==0&&N%3!=0&&N%7!=0)
+    {
+        printf("Plang");
+        }
+        else if (N%7==0&&N%3!=0&&N%5!=0)
+        {
+            printf("Plong");
+            }
+            else if (N%3==0&&N%5==0&&N%7!=0)    
+            {  
+                printf("PlingPlang");
+                }
+                else if (N%3!=0&&N%5==0&&N%7==0)    
+                {
+                    printf("PlangPlong");
+                }
+                else if (N%3==0&&N%5!=0&&N%7==0)
+                {
+                    printf("PlingPlong");
+                }
+                else if (N%3!=0&&N%5!=0&&N%7!=0)
+                {
+                    printf("%d",N);
+                }
+}
